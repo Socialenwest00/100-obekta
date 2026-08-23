14d6ff3447c695229f5e5bc51ef9f4a456d45278
Socialenwest00 <socialwest00@gmail.com>
2026-08-23 10:52:06 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const APP_INFO = {
+  name: "100 НТО",
+  version: "3.0",
+  updated: "2026"
+};
+
+const BADGES = [
+  {
+    visits: 25,
+    name: "Бронзова значка",
+    icon: "🥉"
+  },
+  {
+    visits: 50,
+    name: "Сребърна значка",
+    icon: "🥈"
+  },
+  {
+    visits: 100,
+    name: "Златна значка",
+    icon: "🥇"
+  }
+];
